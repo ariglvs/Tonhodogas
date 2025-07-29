1d3b5bb07b17e1c44e45aab5356916fea608c00c
matheusbrbrb <mmttss1133@gmail.com>
2025-07-29 12:14:04 -0300

    dia 29

--- Tonhodogas/urls.py
+++ Tonhodogas/urls.py
@@ -14,12 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import path
-from core.views import index, perfil
+from django.contrib import admin # type: ignore
+from django.urls import path # type: ignore
+from core.views import index, cadastro
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
-    path('perfil/', perfil, name='perfil'),
+    path('cadastro/', cadastro, name='cadastro'),
 ]
--- core/views.py
+++ core/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render # type: ignore
 
 # Create your views here.
 def  index(request):
     return render(request,"index.html")
 
-def perfil(request):
-    return render(request, "perfil.html")
+def cadastro(request):
+    return render(request, "cadastro.html")
